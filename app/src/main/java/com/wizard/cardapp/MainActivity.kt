c972f7cddc988ee1b554e52d156370d226f4410a
RomanF <r.federov@blanc.ru>
2022-11-18 19:48:42 +0300

    init >

--- app/src/main/java/com/wizard/cardapp/MainActivity.kt
+++ app/src/main/java/com/wizard/cardapp/MainActivity.kt
@@ -3,15 +3,25 @@ package com.wizard.cardapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.wizard.cardapp.ui.theme.WizardCardAppTheme
 
+@ExperimentalMaterial3Api
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,28 +31,32 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    SimpleFilledTextFieldSample()
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
 @Preview(
     showBackground = true,
     showSystemUi = true
 )
+@ExperimentalMaterial3Api
 @Composable
-fun GreetingPreview() {
-    WizardCardAppTheme {
-        Greeting("Android")
+fun SimpleFilledTextFieldSample() {
+    var selected by remember { mutableStateOf(false) }
+    val color = if (selected) Color.Blue else Color.Yellow
+    Row {
+        Button(
+            colors = ButtonDefaults.buttonColors(
+                containerColor = color,
+                contentColor = Color.Black
+            ),
+            onClick = {
+                selected = !selected
+            }) {
+            Text(text = "Hello Android")
+        }
     }
 }

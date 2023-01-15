37129b4914f60b5ef08562d2d1ea194f270f49dd
Packetic <bariy.litievich@gmail.com>
2023-01-15 21:09:56 +0300

    создан прототип экрана подробностей карточки

--- app/src/main/java/com/wizard/cardapp/MainActivity.kt
+++ app/src/main/java/com/wizard/cardapp/MainActivity.kt
@@ -3,23 +3,30 @@ package com.wizard.cardapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.wizard.cardapp.ui.theme.WizardCardAppTheme
+import com.wizard.cardapp.ui.theme.montserrat
 
 @ExperimentalMaterial3Api
 class MainActivity : ComponentActivity() {
@@ -32,7 +39,7 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
 
-                    SimpleFilledTextFieldSample()
+                    ScreenDetails()
                 }
             }
         }
@@ -45,19 +52,96 @@ class MainActivity : ComponentActivity() {
 )
 @ExperimentalMaterial3Api
 @Composable
-fun SimpleFilledTextFieldSample() {
-    var selected by remember { mutableStateOf(false) }
-    val color = if (selected) Color.Blue else Color.Yellow
-    Row {
-        Button(
-            colors = ButtonDefaults.buttonColors(
-                containerColor = color,
-                contentColor = Color.Black
-            ),
-            onClick = {
-                selected = !selected
-            }) {
-            Text(text = "Hello Android")
+fun ScreenDetails() {
+
+    val localDensity = LocalDensity.current
+
+    var columnIconBackHeightDp by remember {
+        mutableStateOf(0.dp)
+    }
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(start = 24.dp, end = 24.dp, top = 15.dp)
+    ) {
+
+        // Hat
+        Row(
+            modifier = Modifier.onGloballyPositioned { coordinates ->
+               columnIconBackHeightDp = with(localDensity) { coordinates.size.height.toDp() }
+            }
+        ) {
+
+            // Icon back
+            Column(
+                modifier = Modifier.width(24.dp).height(columnIconBackHeightDp),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Icon(
+                    painter = painterResource(
+                        id = R.drawable.ic_baseline_arrow_back_ios_24
+                    ),
+                    contentDescription = "Back",
+                )
+            }
+
+            // Title
+            Column(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                Text(
+                    text = "Детали карточки",
+                    fontFamily = montserrat,
+                    fontWeight = FontWeight.SemiBold,
+                    fontSize = 22.sp,
+                    color = Color.Black
+                )
+            }
+        }
+
+        // Card image
+        Row(modifier = Modifier.padding(top = 85.dp)) {
+            Column(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.card_sample),
+                    contentDescription = "Card",
+                    modifier = Modifier
+                        .clip(RoundedCornerShape(15.dp))
+                        .shadow(5.dp)
+                        .padding(28.dp)
+                        .width(300.dp)
+                        .height(280.dp),
+                )
+            }
+        }
+
+        // Character name
+        Row(modifier = Modifier.padding(top = 437.dp, start = 16.dp)) {
+            Text(
+                text = "Имя фамилия",
+                fontFamily = montserrat,
+                fontWeight = FontWeight.SemiBold,
+                fontSize = 16.sp,
+                color = Color.Black
+            )
+        }
+
+        // Character description
+        Row(modifier = Modifier.padding(top = 460.dp, start = 16.dp)) {
+            Text(
+                text = "Описание жизни волшебника",
+                fontFamily = montserrat,
+                fontWeight = FontWeight.Medium,
+                fontSize = 14.sp,
+                color = Color.Gray
+            )
         }
     }
 }
--- app/src/main/java/com/wizard/cardapp/ui/theme/Type.kt
+++ app/src/main/java/com/wizard/cardapp/ui/theme/Type.kt
@@ -2,15 +2,27 @@ package com.wizard.cardapp.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.wizard.cardapp.R
 
 // Set of Material typography styles to start with
+
+val montserrat = FontFamily(
+    listOf(
+        Font(R.font.montserrat_bold, FontWeight.Bold),
+        Font(R.font.montserrat_semibold, FontWeight.SemiBold),
+        Font(R.font.montserrat_medium, FontWeight.Medium),
+        Font(R.font.montserrat_light, FontWeight.Light),
+    )
+)
+
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
+        fontFamily = montserrat,
+        fontWeight = FontWeight.Medium,
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
